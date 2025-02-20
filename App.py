6481a978beba172b767854f02109a6a685cba2d6
Pradabdao <pxdtho.14@gmail.com>
2025-02-20 13:25:00 +0700

    Add files via upload

--- App.py
+++ App.py
@@ -26,8 +26,8 @@ def process_text_file(uploaded_file):
             finish_yds = line[94:104].strip()
             dye_lot = line[105:115].strip()
             grd = line[116:119].strip()
-            last_act_date = line[120:131].strip()
-            wo_no = line[132:136].strip()
+            last_act_date = line[120:129].strip()
+            wo_no = line[130:136].strip()
             print_code = line[137:143].strip()
             shipment = line[144:].strip()
 
